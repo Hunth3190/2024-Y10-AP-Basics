37e06eba81f9533899554e0b13eb872ce8b6fd16
Hunth3190 <hunth3190@masseyhigh.school.nz>
2024-05-21 14:45:05 +1200

    Variable basics

---
+++ P_02_variables.py
@@ -0,0 +1,13 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greetings = "hello world"
+
+# Variables can be numbers
+fav_num = 8
+num_string = "5"
+
+# constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
