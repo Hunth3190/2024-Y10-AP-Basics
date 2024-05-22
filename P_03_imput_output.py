9acbc995bb097b996863a5889e5d3ddc1e7cf64b
Hunth3190 <hunth3190@masseyhigh.school.nz>
2024-05-23 09:58:53 +1200

    code that doubles halves and squares

---
+++ P_03_imput_output.py
@@ -0,0 +1,22 @@
+# ask the user for their name
+username = input("What is your name? ")
+
+# ask the user for their favourite number
+fav_num = int(input("What is your favourite number? "))
+
+# double, half and square the users favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+# Greet the user
+print (f"Hi {username}, your favourite is {fav_num}")
+
+# Output the results of doubling, halving and squaring their favourite integer
+# squaring their favorite integer
+print(f"Double {fav_num} is {double}")
+print(f"halve {fav_num} is {halve}")
+print(f"square {fav_num} is {square}")
+print()
+print("Have a nice day")
+
